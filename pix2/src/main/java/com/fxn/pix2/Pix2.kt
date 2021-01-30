ccabf3f472e5ee9b31b4df80660e6ab5515efd5f
Akshay Sharma <fxn769@gmail.com>
2021-01-30 23:54:32 +0530

    init Pix2

---
+++ pix2/src/main/java/com/fxn/pix2/Pix2.kt
@@ -0,0 +1,11 @@
+package com.fxn.pix2
+
+import android.app.Activity
+import android.os.Bundle
+
+class Pix2 : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pix2)
+    }
+}
